4416eff0b4e2b5c5e9a1b7f4e5ea4c7dab3eb510
pashalis <pascal.frag@gmail.com>
2018-09-19 10:21:54 +0300

    Add Settings Fragment

--- app/src/main/java/com/streetapp/SettingsFragment.java
+++ app/src/main/java/com/streetapp/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.streetapp;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,4 +13,12 @@ public class SettingsFragment extends Fragment {
     public SettingsFragment() {
         // Required empty public constructor
     }
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.settings, container, false);
+
+		return rootView;
+	}
 }
